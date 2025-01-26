303804decd4c0ccf973ffa85d86f1285ec85dfa7
Mogu <riskymuhamad352@gmail.com>
2025-01-26 14:58:15 +0700

    run javascript, variabel, tipe data

---
+++ javascript dasar/run java script/externalfile.js
@@ -0,0 +1,4 @@
+// Akan muncul di tab console browser
+console.log("Halo saya external file dari javascript");
+
+alert("lihat aku deh");
---
+++ javascript dasar/tipe data/script.js
@@ -0,0 +1,23 @@
+let dataString  = "Belajar Javascript"; // String
+console.log(dataString);
+console.log(typeof dataString);
+
+let dataNumber = 10.40; // Number
+console.log(dataNumber);
+console.log(typeof dataNumber);
+
+let dataBoolean = true; // Boolean
+console.log(dataBoolean);   
+console.log(typeof dataBoolean);
+
+dataString = 20; 
+console.log(dataString);
+console.log(typeof dataString);
+// bisa diubah tipe datanya
+
+let dataKosong; // Undefined
+console.log(dataKosong);
+console.log(typeof dataKosong);
+dataKosong = "Aku adalah string";
+console.log(dataKosong);
+console.log(typeof dataKosong);
